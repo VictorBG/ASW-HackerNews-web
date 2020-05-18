041cfb45ad61e4f683bb0dc971fce81ae21486b2
Sergio Vázquez <sergiovazquezmontesdeoca@gmail.com>
2020-05-18 11:21:03 +0200

    User + karma added on main screen

--- src/common/components/toolbar.js
+++ src/common/components/toolbar.js
@@ -1,24 +1,37 @@
-import { TopAppBar, TopAppBarFixedAdjust, TopAppBarRow, TopAppBarSection } from '@rmwc/top-app-bar'
+import {
+  TopAppBar,
+  TopAppBarFixedAdjust,
+  TopAppBarRow,
+  TopAppBarSection
+} from '@rmwc/top-app-bar'
 import React from 'react'
 import styled from 'styled-components'
-import { Button } from '@rmwc/button'
+import {Button} from '@rmwc/button'
+import {useSelector} from "react-redux";
 
-export const Toolbar = ({ children, showUser = true }) => (<>
+export const Toolbar = ({children, showUser = true}) => {
+
+  const user = useSelector((state) => state.user)
+
+  return (<>
         <TopAppBar fixed>
-            <TopAppBarRow>
-                <TopAppBarSectionFull>
-                    {children}
-                </TopAppBarSectionFull>
-                {!!showUser &&
-                <TopAppBarSection alignEnd>
-                    <StyledUsernameText label='(123) VictorBG' theme={['onSecondary']}/>
-                </TopAppBarSection>
-                }
-            </TopAppBarRow>
+          <TopAppBarRow>
+            <TopAppBarSectionFull>
+              {children}
+            </TopAppBarSectionFull>
+            {!!showUser &&
+            <TopAppBarSection alignEnd>
+              <StyledUsernameText label= {`${user.username} (${user.karma})`}
+                                  theme={['onSecondary']}/>
+            </TopAppBarSection>
+            }
+          </TopAppBarRow>
         </TopAppBar>
         <TopAppBarFixedAdjust/>
-    </>
-)
+      </>
+  )
+
+}
 
 const TopAppBarSectionFull = styled(TopAppBarSection)`
     width: 100vw;
