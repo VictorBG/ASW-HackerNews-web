d697c91acde88e74d92b3977ad6fb7f5922d0e7a
Vmorais22 <victormoraisllahi@gmail.com>
2020-05-21 12:01:32 +0200

    -Added handler, watchers and api calls Upvote & Unvote

--- src/modules/lists/duckies/index.js
+++ src/modules/lists/duckies/index.js
@@ -6,6 +6,8 @@ import {typesFor} from "../../../common/utils/network/types-for";
 
 
 export const FETCH_LIST = 'FETCH_LIST'
+export const UPVOTE_CONTRIBUTION = 'UPVOTE_CONTRIBUTION'
+export const UNVOTE_CONTRIBUTION = 'UNVOTE_CONTRIBUTION'
 
 export const fetchList = index => ({ type: FETCH_LIST, index })
 
@@ -26,14 +28,22 @@ export function * listsSaga () {
     function * watchFetchList () {
         yield takeLatest(FETCH_LIST, handleFetchList)
     }
+    function * watchUpVote () {
+        yield takeLatest(UPVOTE_CONTRIBUTION, handleUpVote)
+    }
+    function * watchUnVote () {
+        yield takeLatest(UNVOTE_CONTRIBUTION, handleUnVote)
+    }
+
     yield fork(watchFetchList)
+    yield fork(watchUpVote)
+    yield fork (watchUnVote)
 }
 
-export const UPVOTE_CONTRIBUTION = 'UPVOTE_CONTRIBUTION'
-export const UNVOTE_CONTRIBUTION = 'UNVOTE_CONTRIBUTION'
+
 
 export function * handleUpVote ({ item }) {
-    yield put(apiCall(UPVOTE_CONTRIBUTION, "/likes"), item.id, item.user.username)
+    yield put(apiCall(UPVOTE_CONTRIBUTION, "/likes"), item.id, item.user.username).create()
     // Para determinar si ha habido exito o no al lanzar la petición.
     const {createSuccess, createError} = typesFor(UPVOTE_CONTRIBUTION)
     const [success] = yield race([take(createSuccess), take(createError)])
@@ -43,7 +53,7 @@ export function * handleUpVote ({ item }) {
 }
 
 export function * handleUnVote ({ item }) {
-    yield put(apiCall(UNVOTE_CONTRIBUTION, "/likes"), item.id, item.user.username)
+    yield put(apiCall(UNVOTE_CONTRIBUTION, "/likes"), item.id, item.user.username).delete()
     // Para determinar si ha habido exito o no al lanzar la petición.
     const {createSuccess, createError} = typesFor(UNVOTE_CONTRIBUTION)
     const [success] = yield race([take(createSuccess), take(createError)])
